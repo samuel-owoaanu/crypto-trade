0f9aafad518cf923a3158cbd309653b1206a98b2
samuel-owoaanu <sam.adebayoadesina@gmail.com>
2023-10-13 12:33:45 +0100

    moved the head tag into the header.php file

--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -1,3 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="images/favicon.png" type="">
+    <!-- get the title from backend code -->
+    <title> <?php echo $pageTitle ?> </title>
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href='public/css/bootstrap.css' />
+
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+    <!--owl slider stylesheet -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+    <!-- font awesome style -->
+    <link href="public/css/font-awesome.min.css" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="public/css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="public/css/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+    <div class="hero_area">
+
+        <div class="hero_bg_box">
+            <div class="bg_img_box">
+                <img src="public/images/hero-bg.png" alt="">
+            </div>
+        </div>
+
+
 <!-- header section strats -->
 <header class="header_section">
   <div class="container-fluid">
